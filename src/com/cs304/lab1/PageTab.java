9d463f9594afa2a5bef7b6c5057234c8a92b309b
abdelghanycu <73040423+abdelghanycu@users.noreply.github.com>
2021-11-22 08:50:37 +0200

    Update PageTab.java
    Add comment

--- src/com/cs304/lab1/PageTab.java
+++ src/com/cs304/lab1/PageTab.java
@@ -22,6 +22,8 @@ import javax.swing.WindowConstants;
 
 public class PageTab extends JFrame implements ActionListener, MouseMotionListener, MouseListener,
     KeyListener {
+        
+        // add line
 
 
   private final JPanel jPanel;
