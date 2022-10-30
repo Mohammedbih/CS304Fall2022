0efbc4314900ff962bc5690a9c4a4764fa6549b9
Moody <asdas201888@gmail.com>
2022-10-30 22:38:47 +0200

    format

--- src/com/cs304/templates/simple/SimpleApp.java
+++ src/com/cs304/templates/simple/SimpleApp.java
@@ -6,24 +6,24 @@ import javax.swing.JFrame;
 
 public class SimpleApp extends JFrame {
 
-  private GLCanvas glcanvas;
-  private SimpleGLEventListener listener = new SimpleGLEventListener();
+    private GLCanvas glcanvas;
+    private SimpleGLEventListener listener = new SimpleGLEventListener();
 
-  public static void main(String[] args) {
-    new SimpleApp();
-  }
+    public static void main(String[] args) {
+        new SimpleApp();
+    }
 
-  public SimpleApp() {
-    super("Simple Application");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public SimpleApp() {
+        super("Simple Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    glcanvas = new GLCanvas();
-    glcanvas.addGLEventListener(listener);
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
 
-    add(glcanvas, BorderLayout.CENTER);
-    setSize(500, 300);
-    setLocationRelativeTo(this);
-    setVisible(true);
-  }
+        add(glcanvas, BorderLayout.CENTER);
+        setSize(500, 300);
+        setLocationRelativeTo(this);
+        setVisible(true);
+    }
 
 }
